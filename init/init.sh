6b5b360e46e04037f57c26e12515151efd3d0683
stefanmonkey <csmonkey1439@yahoo.com.cn>
2013-10-25 09:19:24 +0800

    add a init.sh for the environment deploy

--- init/init.sh
+++ init/init.sh
@@ -1,30 +1,27 @@
 #/bin/bash
-#add ubuntu source
-deb http://ppa.launchpad.net/nginx/stable/ubuntu lucid main
-apt-get update -y
 
+#add ubuntu source
+echo "deb http://ppa.launchpad.net/nginx/stable/ubuntu lucid main" >> /etc/apt/sources.list
+apt-get update
+
+#add the key auth
+apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 00A6F0A3C300EE8C
+apt-get update
+
+###############################################################################
 #install nginx
 apt-get install nginx -y
 
+#install mysql
+apt-get install mysql-server -y
+
+
 #install php5-fpm
 aptitude install python-software-properties -y
+add-apt-repository ppa:l-mierzwa/lucid-php5
+aptitude update -y
 
-apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 A42227CB8D0DC64F
-
-add-apt-repository ppa:brianmercer/php5
-
-aptitude -y install php5-cli \
- 		php5-common\
- 		php5-mysql \
- 		php5-suhosin \
- 		php5-gd \
- 		php5-mcrypt \
- 		php5-fpm \
- 		php5-cgi \
- 		php-pear \
- 		php5-curl \
- 		php5-openssl \
- 		php5-dev
+aptitude -y install php5-cli php5-common php5-mysql php5-suhosin php5-gd php5-mcrypt php5-fpm php5-cgi php-pear php5-curl php5-openssl php5-dev
 
 #install apc
 cd /tmp
@@ -37,7 +34,7 @@ phpize
 ./configure --enable-apc --enable-apc-mmap  config=/usr/local/bin/php-config
 make && make install
 
-#install extension
+#install extension memcache
 cd /tmp
 apt-get -y install g++ libevent-dev libcloog-ppl-dev
 wget ftp://ftpuser:5tgb6yhn@59.175.238.4/memcached-2.1.0.tgz
@@ -69,20 +66,24 @@ phpize
 make && make install
 
 
-mv /etc/php5/fpm/php.ini /etc/php5/cli/php.ini.bak
+cp /etc/php5/fpm/php.ini /etc/php5/cli/php.ini.bak
 
-vi /etc/php5/fpm/php.ini
-extension=igbinary.so
-extension=memcached.so
-
-
-
+echo "extension=igbinary.so" >> /etc/php5/fpm/php.ini
+echo "extension=memcached.so" >> /etc/php5/fpm/php.ini
+echo "extension=apc.so" >> /etc/php5/fpm/php.ini
+echo "apc.shm_size=250m" >> /etc/php5/fpm/php.ini
+echo "cgi.fix_pathinfo = 0" >> /etc/php5/fpm/php.ini
+###############################################################################
 
 #unmark the "#"
 sed -n 's/#/;/g' /etc/php5/fpm/conf.d/mcrypt.ini
 
-/etc/nginx/sites-available/default
-php5_default=
+#build directory
+mkdir -p /var/www/authserver  
+mkdir -p /var/www/herouser
+
+/etc/nginx/sites-available/default 
+default_first=
 """
 #configure the nginx and php
 
@@ -90,12 +91,9 @@ server {
         listen   80 default;
         server_name  localhost;
 
-        access_log  /var/log/nginx/localhost.access.log;
-
-        location / {
-                root   /var/www/html;
-                index  index.php index.html index.htm;
-        }
+        access_log  /var/log/nginx/localhost.80.access.log;
+		root   /var/www/authserver;
+		index  index.php index.html index.htm;
 
         location /doc {
                 root   /usr/share;
@@ -110,17 +108,13 @@ server {
         }
 
         location ~ \.php$ {
-                fastcgi_pass   127.0.0.1:9000;
-                fastcgi_index  index.php;
-                fastcgi_param  SCRIPT_FILENAME  /var/www/html$fastcgi_script_name;
-                include fastcgi_params;
+            fastcgi_pass   	127.0.0.1:9000;
+            fastcgi_index  	index.php;
+            fastcgi_params  SCRIPT_FILENAME  /var/www/authserver$fastcgi_script_name;
+            include 		fastcgi_params;
         }
 }
 """
-
-#install memcache
-pecl install memcache
-
 
 
 /etc/nginx/sites-available/herouser
@@ -128,10 +122,11 @@ herouser=
 """
 server {
 	listen 81;
+	server_name localhost;
+
+	access_log  /var/log/nginx/localhost.81.access.log;
 	root /var/www/herouser;
 	index index.php index.html index.htm;
-
-	server_name localhost;
 
 	location /doc {
 		root /usr/share;
@@ -146,14 +141,52 @@ server {
 	}
 
 	location ~ \.php$ {
-		fastcgi_pass 127.0.0.1:9000;
-		fastcgi_index index.php;
-		fastcgi_param SCRIPT_FILENAME /var/www/herouser$fastcgi_script_name;
-		include fastcgi_params;
+		fastcgi_pass 	127.0.0.1:9000;
+		fastcgi_index 	index.php;
+		fastcgi_params	SCRIPT_FILENAME /var/www/herouser$fastcgi_script_name;
+		include 		fastcgi_params;
 	}
 }
 
 """
 
-ol_serverlist
+ln -s /etc/nginx/sites-available/herouser /etc/nginx/sites-enabled/herouser
 
+
+
+#install memcache
+pecl install memcache<<EOF
+
+
+EOF
+apt-get install memcached
+
+/usr/bin/memcached -m 64 -p 11211 -u nobody -l 127.0.0.1
+
+#hero mysql init
+mysql -u root -p123456 <<EOF
+create database hero character set utf8;
+create user hero identified by '123456';
+grant all on *.* to hero identified by '123456';
+
+source /var/www/herouser/hero.sql
+source /var/www/herouser/append.sql
+source /var/www/herouser/ios_app_pay.sql
+source /var/www/herouser/pay.sql
+quit
+EOF
+
+#authserver mysql init
+mysql -u root -p123456 <<EOF
+source /var/www/authserver/authServer/authserver.sql
+source /var/www/authserver/authServer/append.sql
+
+create user auth identified by '123456';
+grant all on *.* to auth identified by '123456';
+insert into ol_serverlist values ("","test","192.168.1.203:81","","1","","0","1","1.36");
+quit
+EOF
+
+
+mysql
+ol_serverlist
