12f4409270fb42072afac9d30927b0600a1fdbcb
stefanmonkey <stefan_bo@163.com>
2013-11-05 17:13:34 +0800

    django test

--- django_development/mysite/views.py
+++ django_development/mysite/views.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse
+#from django.template.loader import get_template
+#from django.template import Context
+#from django.http import HttpResponse
 import datetime
+from django.shortcuts import render_to_response
 
 def hello(request):
 	return HttpResponse("Hello world")
@@ -7,10 +10,19 @@ def hello(request):
 def my_homepage_view(request):
 	return HttpResponse("It work!")
 
+#def current_datetime(request):
+#	now = datetime.datetime.now()
+#	t = get_template('current_datetime.html')
+#	html = t.render(Context({'current_date':now}))
+#	return HttpResponse(html)
+
+#def current_datetime(request):
+#	now = datetime.datetime.now()
+#	return render_to_response('current_datetime.html',{'current_date': now})
+
 def current_datetime(request):
-	now = datetime.datetime.now()
-	html = "<html><body>It is now %s.</body></html>" % now
-	return HttpResponse(html)
+	current_date = datetime.datetime.now()
+	return render_to_response('current_datetime.html',locals())
 
 def hours_ahead(request, offset):
 	try:
