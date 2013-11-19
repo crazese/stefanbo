63f7448edaf476919e1957128431a15bf1d3f8d9
stefanmonkey <stefan_bo@163.com>
2013-11-19 16:34:49 +0800

    add lnmp sh

---
+++ tools/lnmp+memcache/init/init_ubuntu.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+#init system on ubuntu server 10.04
+# update the apt-get sourcelist to 163 source and backup the previous list
+cp /etc/apt/sources.list /etc/apt/sources.list.bak 
+cat > /etc/apt/sources.list <<EOF
+deb http://mirrors.163.com/ubuntu/ precise main universe restricted multiverse
+deb http://mirrors.163.com/ubuntu/ precise-security universe main multiverse restricted
+deb http://mirrors.163.com/ubuntu/ precise-updates universe main multiverse restricted
+deb http://mirrors.163.com/ubuntu/ precise-proposed universe main multiverse restricted
+deb http://mirrors.163.com/ubuntu/ precise-backports universe main multiverse restricted
+EOF
+
+apt-get -y update
+
+apt-get -y remove apache2 apache2-doc apache2-utils apache2.2-common apache2.2-bin apache2-mpm-prefork apache2-doc apache2-mpm-worker mysql-client mysql-server mysql-common php5 php5-common php5-cgi php5-mysql php5-curl php5-gd
+dpkg -p apache2 apache2-doc apache2-mpm-prefork apache2-utils apache2.2-common libmysqlclient15off libmysqlclient15-dev mysql-common php5 php5-common php5-cgi php5-mysql php5-curl php5-gd
+
+dpkg -l |grep mysql 
+dpkg -P libmysqlclient15off libmysqlclient15-dev mysql-common 
+dpkg -l |grep apache 
+dpkg -P apache2 apache2-doc apache2-mpm-prefork apache2-utils apache2.2-common
+dpkg -l |grep php 
+dpkg -P php5 php5-common php5-cgi php5-mysql php5-curl php5-gd
+apt-get purge `dpkg -l | grep php| awk '{print $2}'`
+
+# Install needed packages
+apt-get -y install gcc g++ make autoconf libjpeg8 libjpeg8-dev libpng12-0 libpng12-dev libpng3 libfreetype6 libfreetype6-dev libxml2 libxml2-dev zlib1g zlib1g-dev libc6 libc6-dev libglib2.0-0 libglib2.0-dev bzip2 libzip-dev libbz2-1.0 libncurses5 libncurses5-dev curl libcurl3 libcurl4-openssl-dev e2fsprogs libkrb5-3 libkrb5-dev libltdl-dev libidn11 libidn11-dev openssl libtool libevent-dev slapd ldap-utils libnss-ldap bison libsasl2-dev libxslt1-dev vim zip unzip tmux htop wget
+
+if [ ! -z "`cat /etc/issue | grep 13`" ];then
+       apt-get -y install libcloog-ppl1
+elif [ ! -z "`cat /etc/issue | grep 10`" ];then
+       apt-get -y install libcloog-ppl0
+fi
+
+# history size 
+sed -i 's/HISTSIZE=.*$/HISTSIZE=100/g' ~/.bashrc 
+[ -z "`cat ~/.bashrc | grep history-timestamp`" ] && echo "export PROMPT_COMMAND='{ msg=\$(history 1 | { read x y; echo \$y; });user=\$(whoami); echo \$(date \"+%Y-%m-%d %H:%M:%S\"):\$user:\`pwd\`/:\$msg ---- \$(who am i); } >> /tmp/\`hostname\`.\`whoami\`.history-timestamp'" >> ~/.bashrc
+
+# /etc/security/limits.conf
+[ -z "`cat /etc/security/limits.conf | grep 'nproc 65535'`" ] && cat >> /etc/security/limits.conf <<EOF
+* soft nproc 65535
+* hard nproc 65535
+* soft nofile 65535
+* hard nofile 65535
+EOF
+[ -z "`cat /etc/rc.local | grep 'ulimit -SH 65535'`" ] && echo "ulimit -SH 65535" >> /etc/rc.local
+
+# /etc/hosts
+[ "$(hostname -i | awk '{print $1}')" != "127.0.0.1" ] && sed -i "s@^127.0.0.1\(.*\)@127.0.0.1   `hostname` \1@" /etc/hosts
+
+# Set timezone
+rm -rf /etc/localtime
+ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
+# Set OpenDNS
+if [ ! -z "`cat /etc/resolv.conf | grep '8\.8\.8\.8'`" ];then
+cat > /etc/resolv.conf << EOF
+nameserver 192.168.1.8
+nameserver 202.103.44.150
+EOF
+fi
+
+# /etc/sysctl.conf
+[ -z "`cat /etc/sysctl.conf | grep 'fs.file-max'`" ] && cat >> /etc/sysctl.conf << EOF
+net.ipv4.tcp_syncookies = 1
+fs.file-max=65535
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_tw_recycle = 1
+net.ipv4.ip_local_port_range = 1024 65000
+EOF
+sysctl -p
+
+# Update time
+ntpdate pool.ntp.org 
+echo '*/20 * * * * /usr/sbin/ntpdate pool.ntp.org > /dev/null 2>&1' > /var/spool/cron/crontabs/root;chmod 600 /var/spool/cron/crontabs/root 
+service cron restart
+
+. ~/.bashrc
+
+# mkdir the Install directory
+mkdir /opt/lnmp/php -p
+mkdir /opt/lnmp/mysql -p
+mkdir /opt/lnmp/nginx -p
+mkdir /opt/lnmp/memcached -p
+mkdir /opt/lnmp/tar_package -p
+
+#Disable SeLinux
+if [ -s /etc/selinux/config ]; then
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+fi
+
+if [ -s /etc/ld.so.conf.d/libc6-xen.conf ]; then
+sed -i 's/hwcap 1 nosegneg/hwcap 0 nosegneg/g' /etc/ld.so.conf.d/libc6-xen.conf
---
+++ tools/lnmp+memcache/install.sh
@@ -0,0 +1,274 @@
+#!/bin/bash
+# Author: zhubo
+
+# Check if user is root
+[ $(id -u) != "0" ] && echo "Error: You must be root to run this script" && exit 1 
+
+export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+clear
+printf "
+#######################################################################
+#	php-5.3.10 		                                                  #
+#	extentsion(cli,mysql,gd,mcrypt,fpm,curl,openssl,memcached,apc)    #
+#	nginx-1.0.9                                                       #
+# 	mysql Ver 14.12 Distrib 5.0.27                                    #
+#	memcached-1.5                                                     #
+#	安装过程中所需要的第三方类库都安装在对应的路径下，例如：          #
+#	Php扩展apc所需的libpcre3，需要编译安装至php所在目录。             #
+#	目录设定如下:                                                     #
+#	Php  /opt/lnmp/php                                                #
+#	Mysql  /opt/lnmp/mysql                                            #
+#	Nginx  /opt/lnmp/nginx                                            #
+#	Memcached  /opt/lnmp/memcached                                    #
+#                                                                     #
+#######################################################################"
+cur_dir=$(pwd)
+
+
+#set mysql root password
+mysqlrootpwd="123456"
+installinnodb="y"
+isinstallphp53="y"
+isinstallmysql50="n"
+
+#get tar.gz or tgz from ftp server
+function ftp_get(){
+server="hut.jofgame.com"
+username="zhubo"
+passwd="Jof1Game8yzl"
+
+cd /opt/lnmp/tar_package
+
+ftp -n $server <<EOF
+prompt off
+user $username $passwd
+lcd lnmp
+cd lnmp/tar_package
+mget * 
+bye
+EOF
+}
+
+
+function InstallDependsAndOpt()
+{
+cd $cur_dir
+
+tar zxvf autoconf-2.13.tar.gz
+cd autoconf-2.13/
+./configure --prefix=/usr/local/autoconf-2.13
+make && make install
+cd ../
+
+cd $cur_dir
+tar zxvf libiconv-1.14.tar.gz
+cd libiconv-1.14/
+./configure
+make && make install
+cd ../
+
+cd $cur_dir
+tar zxvf libmcrypt-2.5.8.tar.gz
+cd libmcrypt-2.5.8/
+./configure
+make && make install
+/sbin/ldconfig
+cd libltdl/
+./configure --enable-ltdl-install
+make && make install
+cd ../../
+
+ln -s /usr/local/lib/libmcrypt.la /usr/lib/libmcrypt.la
+ln -s /usr/local/lib/libmcrypt.so /usr/lib/libmcrypt.so
+ln -s /usr/local/lib/libmcrypt.so.4 /usr/lib/libmcrypt.so.4
+ln -s /usr/local/lib/libmcrypt.so.4.4.8 /usr/lib/libmcrypt.so.4.4.8
+
+cd $cur_dir
+tar zxvf libxml2-2.7.8.tar.gz
+cd libxml2-2.7.8/
+./configure --prefix=/usr
+make && make install
+cd ../
+
+if [ `getconf WORD_BIT` = '32' ] && [ `getconf LONG_BIT` = '64' ] ; then
+        ln -s /usr/lib/x86_64-linux-gnu/libpng* /usr/lib/
+        ln -s /usr/lib/x86_64-linux-gnu/libjpeg* /usr/lib/
+else
+        ln -s /usr/lib/i386-linux-gnu/libpng* /usr/lib/
+        ln -s /usr/lib/i386-linux-gnu/libjpeg* /usr/lib/
+fi
+
+ulimit -v unlimited
+
+if [ ! `grep -l "/lib"    '/etc/ld.so.conf'` ]; then
+	echo "/lib" >> /etc/ld.so.conf
+fi
+
+if [ ! `grep -l '/usr/lib'    '/etc/ld.so.conf'` ]; then
+	echo "/usr/lib" >> /etc/ld.so.conf
+fi
+
+if [ -d "/usr/lib64" ] && [ ! `grep -l '/usr/lib64'    '/etc/ld.so.conf'` ]; then
+	echo "/usr/lib64" >> /etc/ld.so.conf
+fi
+
+if [ ! `grep -l '/usr/local/lib'    '/etc/ld.so.conf'` ]; then
+	echo "/usr/local/lib" >> /etc/ld.so.conf
+fi
+
+ldconfig
+
+cat >>/etc/security/limits.conf<<eof
+* soft nproc 65535
+* hard nproc 65535
+* soft nofile 65535
+* hard nofile 65535
+eof
+
+cat >>/etc/sysctl.conf<<eof
+fs.file-max=65535
+eof
+}
+
+
+function InstallNginx()
+{
+echo "============================Install Nginx================================="
+groupadd www
+useradd -s /sbin/nologin -g www www
+
+mkdir -p /opt/wwwroot/default
+chmod +w /opt/wwwroot/default
+mkdir -p /opt/wwwlogs
+chmod 777 /opt/wwwlogs
+touch /opt/wwwlogs/nginx_error.log
+
+cd $cur_dir
+chown -R www:www /home/wwwroot/default
+
+# nginx
+cd $cur_dir
+tar zxvf pcre-8.12.tar.gz
+cd pcre-8.12/
+./configure
+make && make install
+cd ../
+
+ldconfig
+
+tar zxvf nginx-1.2.7.tar.gz
+cd nginx-1.2.7/
+./configure --user=www --group=www --prefix=/usr/local/nginx --with-http_stub_status_module --with-http_ssl_module --with-http_gzip_static_module --with-ipv6
+make && make install
+cd ../
+
+ln -s /usr/local/nginx/sbin/nginx /usr/bin/nginx
+
+cd $cur_dir
+rm -f /usr/local/nginx/conf/nginx.conf
+cp conf/nginx.conf /usr/local/nginx/conf/nginx.conf
+cp conf/dabr.conf /usr/local/nginx/conf/dabr.conf
+cp conf/discuz.conf /usr/local/nginx/conf/discuz.conf
+cp conf/sablog.conf /usr/local/nginx/conf/sablog.conf
+cp conf/typecho.conf /usr/local/nginx/conf/typecho.conf
+cp conf/wordpress.conf /usr/local/nginx/conf/wordpress.conf
+cp conf/discuzx.conf /usr/local/nginx/conf/discuzx.conf
+cp conf/wp2.conf /usr/local/nginx/conf/wp2.conf
+cp conf/phpwind.conf /usr/local/nginx/conf/phpwind.conf
+cp conf/shopex.conf /usr/local/nginx/conf/shopex.conf
+cp conf/dedecms.conf /usr/local/nginx/conf/dedecms.conf
+cp conf/drupal.conf /usr/local/nginx/conf/drupal.conf
+cp conf/ecshop.conf /usr/local/nginx/conf/ecshop.conf
+rm -f /usr/local/nginx/conf/fcgi.conf
+cp conf/fcgi.conf /usr/local/nginx/conf/fcgi.conf
+}
+
+
+function InstallPHP53()
+{
+echo "============================Install PHP 5.3.17================================"
+cd $cur_dir
+export PHP_AUTOCONF=/usr/local/autoconf-2.13/bin/autoconf
+export PHP_AUTOHEADER=/usr/local/autoconf-2.13/bin/autoheader
+tar zxvf php-5.3.17.tar.gz
+cd php-5.3.17/
+./configure --prefix=/usr/local/php --with-config-file-path=/usr/local/php/etc --enable-fpm --with-fpm-user=www --with-fpm-group=www --with-mysql=mysqlnd --with-mysqli=mysqlnd --with-pdo-mysql=mysqlnd --with-iconv-dir --with-freetype-dir --with-jpeg-dir --with-png-dir --with-zlib --with-libxml-dir=/usr --enable-xml --disable-rpath --enable-magic-quotes --enable-safe-mode --enable-bcmath --enable-shmop --enable-sysvsem --enable-inline-optimization --with-curl --with-curlwrappers --enable-mbregex --enable-mbstring --with-mcrypt --enable-ftp --with-gd --enable-gd-native-ttf --with-openssl --with-mhash --enable-pcntl --enable-sockets --with-xmlrpc --enable-zip --enable-soap --without-pear --with-gettext --disable-fileinfo
+
+make ZEND_EXTRA_LIBS='-liconv'
+make install
+
+rm -f /usr/bin/php
+ln -s /usr/local/php/bin/php /usr/bin/php
+ln -s /usr/local/php/bin/phpize /usr/bin/phpize
+ln -s /usr/local/php/sbin/php-fpm /usr/bin/php-fpm
+
+echo "Copy new php configure file."
+mkdir -p /usr/local/php/etc
+cp php.ini-production /usr/local/php/etc/php.ini
+
+cd $cur_dir
+# php extensions
+echo "Modify php.ini......"
+sed -i 's/post_max_size = 8M/post_max_size = 50M/g' /usr/local/php/etc/php.ini
+sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 50M/g' /usr/local/php/etc/php.ini
+sed -i 's/;date.timezone =/date.timezone = PRC/g' /usr/local/php/etc/php.ini
+sed -i 's/short_open_tag = Off/short_open_tag = On/g' /usr/local/php/etc/php.ini
+sed -i 's/; cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /usr/local/php/etc/php.ini
+sed -i 's/; cgi.fix_pathinfo=0/cgi.fix_pathinfo=0/g' /usr/local/php/etc/php.ini
+sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /usr/local/php/etc/php.ini
+sed -i 's/max_execution_time = 30/max_execution_time = 300/g' /usr/local/php/etc/php.ini
+sed -i 's/register_long_arrays = On/;register_long_arrays = On/g' /usr/local/php/etc/php.ini
+sed -i 's/magic_quotes_gpc = On/;magic_quotes_gpc = On/g' /usr/local/php/etc/php.ini
+sed -i 's/disable_functions =.*/disable_functions = passthru,exec,system,chroot,scandir,chgrp,chown,shell_exec,proc_open,proc_get_status,ini_alter,ini_restore,dl,pfsockopen,openlog,syslog,readlink,symlink,popepassthru,stream_socket_server,fsockopen/g' /usr/local/php/etc/php.ini
+
+echo "Install ZendGuardLoader for PHP 5.3"
+if [ `getconf WORD_BIT` = '32' ] && [ `getconf LONG_BIT` = '64' ] ; then
+	wget -c http://downloads.zend.com/guard/5.5.0/ZendGuardLoader-php-5.3-linux-glibc23-x86_64.tar.gz
+	tar zxvf ZendGuardLoader-php-5.3-linux-glibc23-x86_64.tar.gz
+	mkdir -p /usr/local/zend/
+	cp ZendGuardLoader-php-5.3-linux-glibc23-x86_64/php-5.3.x/ZendGuardLoader.so /usr/local/zend/
+else
+	wget -c http://downloads.zend.com/guard/5.5.0/ZendGuardLoader-php-5.3-linux-glibc23-i386.tar.gz
+	tar zxvf ZendGuardLoader-php-5.3-linux-glibc23-i386.tar.gz
+	mkdir -p /usr/local/zend/
+	cp ZendGuardLoader-php-5.3-linux-glibc23-i386/php-5.3.x/ZendGuardLoader.so /usr/local/zend/
+fi
+
+echo "Write ZendGuardLoader to php.ini......"
+cat >>/usr/local/php/etc/php.ini<<EOF
+;eaccelerator
+
+;ionCube
+
+[Zend Optimizer] 
+zend_extension=/usr/local/zend/ZendGuardLoader.so
+EOF
+
+echo "Creating new php-fpm configure file......"
+cat >/usr/local/php/etc/php-fpm.conf<<EOF
+[global]
+pid = /usr/local/php/var/run/php-fpm.pid
+error_log = /usr/local/php/var/log/php-fpm.log
+log_level = notice
+
+[www]
+listen = /tmp/php-cgi.sock
+user = www
+group = www
+pm = dynamic
+pm.max_children = 10
+pm.start_servers = 2
+pm.min_spare_servers = 1
+pm.max_spare_servers = 6
+request_terminate_timeout = 100
+EOF
+
+echo "Copy php-fpm init.d file......"
+cp $cur_dir/php-5.3.17/sapi/fpm/init.d.php-fpm /etc/init.d/php-fpm
+chmod +x /etc/init.d/php-fpm
+
+cp $cur_dir/lnmp /root/lnmp
+chmod +x /root/lnmp
+sed -i 's:/usr/local/php/logs:/usr/local/php/var/run:g' /root/lnmp
+echo "============================PHP 5.3.17 install completed======================"
+}
