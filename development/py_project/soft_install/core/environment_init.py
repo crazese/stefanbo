c822f57040b514e19b2244d6d914d5a572079a88
stefanmonkey <stefan_bo@163.com>
2013-12-16 16:39:11 +0800

    missing file

--- development/py_project/soft_install/core/environment_init.py
+++ development/py_project/soft_install/core/environment_init.py
@@ -17,60 +17,59 @@ deb-src http://mirrors.163.com/ubuntu/ lucid-updates universe main multiverse re
 """
 
 def apt_conf():
-	apt_file=open('/etc/apt/sources.list','w')
-	apt_file.write(apt_conf)
-	apt_file.close
+        apt_file=open('/etc/apt/sources.list','w')
+        apt_file.write(apt_conf)
+        apt_file.close
 
 def apt_update():
-	os_cmd('apt-get update')
-	os_cmd('apt-get -y upgrade')
+        os_cmd('apt-get update')
+        os_cmd('apt-get -y upgrade')
 
 def _install(name):
-	os_cmd('apt-get install -y %s' % name)
-	os_cmd('apt-get -fy install')
-	os_cmd('apt-get -y autoremove')
+        os_cmd('apt-get install -y %s' % name)
+        os_cmd('apt-get -fy install')
+        os_cmd('apt-get -y autoremove')
 
 
 def install_init():
-	apt_f=open('/etc/apt/sources.list','r')
-	count = len(apt_f.readlines())
-	soft_list = ['build-essential',
-				 'gcc',
-				 'g++',
-				 'make',
-				 'libncurses5-dev',
-				 'libxp-dev',
-				 'libmotif-dev',
-				 'libxt-dev',
-				 'libstdc++6']
-	if count != 10:
-		apt_conf()
-	else:
-		apt_update()
-		for soft in soft_list:
-			_install(soft)
+        apt_f=open('/etc/apt/sources.list','r')
+        count = len(apt_f.readlines())
+        soft_list = ['build-essential',
+                                 'gcc',
+                                 'g++',
+                                 'make',
+                                 'libncurses5-dev',
+                                 'libxp-dev',
+                                 'libmotif-dev',
+                                 'libxt-dev',
+                                 'libstdc++6']
+        if count != 10:
+                apt_conf()
+        else:
+                apt_update()
+                for soft in soft_list:
+                        _install(soft)
 
 
 def folder_and_user_init(name,uname):
-	install_base = '/opt/lnmp/app'
-	install_name = os.path.join(install_base, name)
-	tar_base = '/opt/lnmp/tar_package'
-	tar_name = os.path.join(tar_base, name)
-	order = 'id %s' % uname
-	order = shlex.split(order)
-	j_name = subprocess.Popen(order, stdout=subprocess.PIPE)
-	if not (uname in str(j_name.stdout.read())):
-		os.system('groupadd %s && useradd -g %s %s' % (uname, uname, uname))
-	if not(os.path.exists(install_name)):
-		os.makedirs(install_name)
-		t_uid = pwd.getpwnam(uname).pw_uid
-		t_gid = pwd.getpwnam(uname).pw_gid
-		os.chown(install_name, t_uid,t_gid)
-	if not(os.path.exists(tar_name)):
-		os.makedirs(tar_name)
+        install_base = '/opt/lnmp/app'
+        install_name = os.path.join(install_base, name)
+        tar_base = '/opt/lnmp/tar_package'
+        tar_name = os.path.join(tar_base, name)
+        order = 'id %s' % uname
+        order = shlex.split(order)
+        j_name = subprocess.Popen(order, stdout=subprocess.PIPE)
+        if not (uname in str(j_name.stdout.read())):
+                os.system('groupadd %s && useradd -g %s %s' % (uname, uname, uname))
+        if not(os.path.exists(install_name)):
+                os.makedirs(install_name)
+                t_uid = pwd.getpwnam(uname).pw_uid
+                t_gid = pwd.getpwnam(uname).pw_gid
+                os.chown(install_name, t_uid,t_gid)
+        if not(os.path.exists(tar_name)):
+                os.makedirs(tar_name)
 
 #def os_cmd(order):
-#	order = shlex.split(order)
-#	o_name = subprocess.Popen(order,stdout=subprocess.PIPE)
-#	return str(o_name.stdout.read()) 
-
+#        order = shlex.split(order)
+#        o_name = subprocess.Popen(order,stdout=subprocess.PIPE)
+#        return str(o_name.stdout.read()) 
