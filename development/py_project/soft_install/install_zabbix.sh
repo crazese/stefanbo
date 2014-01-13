ad73cee1efa03e0940cf7df3ab3d4d35c919e331
stefanmonkey <stefan_bo@163.com>
2014-01-13 17:04:09 +0800

    need to create zabbix install scripts with salt

--- development/py_project/soft_install/install_zabbix.sh
+++ development/py_project/soft_install/install_zabbix.sh
@@ -16,7 +16,7 @@
 #apt-get install snmp -y
 
 
-
+# install server
 cd /usr/local/lnmp/tar_package/zabbix/
 tar -zxvf zabbix-2.2.1.tar.gz
 cd zabbix-2.2.1
@@ -25,6 +25,16 @@ cd zabbix-2.2.1
 --enable-server \
 --enable-agent \
 --with-mysql \
+--enable-ipv6 \
+--with-net-snmp \
+--with-libcurl \
+--with-libxml2 \
+--with-jabber
+
+# install agent
+./configure \
+--prefix=/usr/local/app/zabbix \
+--enable-agent \
 --enable-ipv6 \
 --with-net-snmp \
 --with-libcurl \
